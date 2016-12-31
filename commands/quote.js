2dbe3421afdfbef3a8a28de4c89c7cd0256af00b
chris062689 <chris062689@gmail.com>
2016-12-31 00:48:39 -0500

    Use arrow functions, code cleanup.

--- commands/quote.js
+++ commands/quote.js
@@ -4,10 +4,12 @@ var logger = require('../logging.js');
 exports.roles = ['Admins', 'Moderators', 'Secret'];
 exports.command = function(message, reply) {
   let replyMessage = 'Hello.';
-  if (reply == null) { replyMessage = message.content.substr(message.content.indexOf(' ') + 1); }
-  else { replyMessage = `${message.mentions.users.map(function (user) { return `${user}`; })} ${reply}`; }
+  if (reply == null) {
+    replyMessage = message.content.substr(message.content.indexOf(' ') + 1);
+  }
+  else {
+    replyMessage = `${message.mentions.users.map(user => `${user}`)} ${reply}`;
+  }
 
-  message.channel.sendMessage(replyMessage).catch(function (error) {
-      logger.error('Unable to send reply message to quote.', error);
-  });
+  message.channel.sendMessage(replyMessage);
 }
