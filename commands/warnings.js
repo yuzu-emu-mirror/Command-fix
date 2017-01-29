8034d2667cbb8ddd8bb44c56f703c19fcec9ccb1
chris062689 <chris062689@gmail.com>
2017-01-29 15:46:54 -0500

    Updated variable name for wearnings.

--- commands/warnings.js
+++ commands/warnings.js
@@ -3,7 +3,7 @@ var logger = require('../logging.js');
 
 exports.command = function(message) {
   message.mentions.users.map((user) => {
-    var count = app.warnings.filter(x => x.id == user.id && !x.cleared).length || 0;
-    message.channel.sendMessage(`${user}, you have ${count} warnings.`);
+    var warnings = app.warnings.filter(x => x.id == user.id && !x.cleared);
+    message.channel.sendMessage(`${user}, you have ${count.warnings} total warnings.`);
   });
 }
