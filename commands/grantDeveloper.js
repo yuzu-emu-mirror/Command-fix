b7693b291691e1450ad22faaa2a30af179c43b1d
chris062689 <chris062689@gmail.com>
2017-08-10 20:14:51 -0400

    Corrected alreadyJoined to point to the role ID, corrected if statement to account for a boolean.  Using sendMessage instead of reply.

--- commands/grantDeveloper.js
+++ commands/grantDeveloper.js
@@ -6,14 +6,14 @@ exports.command = function(message) {
   var role = '345247291843805185';
   message.mentions.users.map((user) => {
     let member = message.guild.member(user);
-    let alreadyJoined = member.roles.has(role.id);
+    let alreadyJoined = member.roles.has(role);
 
-    if (alreadyJoined != null) {
+    if (alreadyJoined) {
       member.removeRole(role);
-      message.reply(`${user}'s speech has been revoked in the #development channel.`);
+      message.channel.sendMessage(`${user}'s speech has been revoked in the #development channel.`);
     } else {
       member.addRole(role);
-      message.reply(`${user} has been granted speech in the #development channel.`);
+      message.channel.sendMessage(`${user} has been granted speech in the #development channel.`);
     }
   });
 }
