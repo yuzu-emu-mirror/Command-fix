0ab62ff4df0ccd1f56292c98b84de07f7d73fd70
liushuyu <liushuyu011@gmail.com>
2022-09-16 18:01:37 -0600

    rollup: remove unneeded exports from the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,34 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 
 import * as loader from "./generateExports";
 loader;
 
 export default {
-    input: 'src/server.ts',
-    external: ['discord.js'],
-    output: {
-        dir: 'dist',
-        format: 'cjs',
-    },
-    plugins: [
-        resolve({ browser: false }),
-        commonjs({ dynamicRequireTargets: [
-            'node_modules/logform/*.js',
-            './src/responses/citra.json',
-            './src/responses/yuzu.json',
-        ], extensions: ['.mjs', '.js', '.ts'], transformMixedEsModules: true }),
-        json(),
-        typescript(),
-        terser()
-    ]
+  input: "src/server.ts",
+  external: ["discord.js"],
+  preserveEntrySignatures: false,
+  output: {
+    dir: "dist",
+    format: "cjs",
+  },
+  plugins: [
+    resolve({ browser: false }),
+    commonjs({
+      dynamicRequireTargets: [
+        "node_modules/logform/*.js",
+        "./src/responses/citra.json",
+        "./src/responses/yuzu.json",
+      ],
+      extensions: [".mjs", ".js", ".ts"],
+      transformMixedEsModules: true,
+      ignoreGlobal: true,
+    }),
+    json(),
+    typescript(),
+    terser(),
+  ],
 };
